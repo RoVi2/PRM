825fec1c752ea92923093542ed16d0221e4a7b55
veimox <veimox@gmail.com>
2015-03-09 18:56:35 +0100

    first commit

---
+++ src/PRM.cpp
@@ -0,0 +1,18 @@
+/*
+ * PRM.cpp
+ *
+ *  Created on: Mar 09, 2015
+ *      Author: Jorge Rodriguez Marin
+ */
+
+#include <iostream>
+#include <rw/rw.hpp>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	cout << "Hola Mundo" << endl;
+	
+	return 0;
+}
